4feb12a710670cfdd0b489a96ecfb7df8da79299
Andres pardo <fernando.andresp13@gmail.com>
2023-11-13 13:27:39 -0500

    se agrego la estructura del proyecto y propiedades jpa

--- src/main/java/com/andres/sistema/blog/SistemaBlogSpringBootApiRestApplication.java
+++ src/main/java/com/andres/sistema/blog/SistemaBlogSpringBootApiRestApplication.java
@@ -1,4 +1,4 @@
-package com.andres.sistema.blog.demo;
+package com.andres.sistema.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/andres/sistema/blog/SistemaBlogSpringBootApiRestApplicationTests.java
+++ src/test/java/com/andres/sistema/blog/SistemaBlogSpringBootApiRestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.andres.sistema.blog.demo;
+package com.andres.sistema.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
